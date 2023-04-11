4914dbac70c143b432848386ca022fc0b3435e5b
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2023-04-11 18:51:37 +0100

    Convert to tokio

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
-use std::io::Write;
-use std::net::{TcpListener, TcpStream};
+use std::net::TcpListener;
+use tokio::io::AsyncWriteExt;
 
-fn handle_stream(mut stream: TcpStream) -> io::Result<()> {
-    stream.write_all(b"+PONG\r\n")?;
-    stream.flush()?;
+async fn handle_stream(mut stream: tokio::net::TcpStream) -> io::Result<()> {
+    stream.write_all(b"+PONG\r\n").await?;
+    stream.flush().await?;
+
     Ok(())
 }
 
@@ -14,9 +15,12 @@ async fn main() -> io::Result<()> {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    println!("line 19 main");
 
     for stream in listener.incoming() {
-        handle_stream(stream?)?;
+        let stream = stream?;
+        stream.set_nonblocking(true)?;
+        handle_stream(tokio::net::TcpStream::from_std(stream)?).await?;
     }
 
     Ok(())
