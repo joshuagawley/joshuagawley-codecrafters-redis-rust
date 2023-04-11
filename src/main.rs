97a7258baec7e8dc3da3b9e731cbaf2115a9c7b9
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2023-04-11 18:42:02 +0100

    Pass second stage

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-use std::net::TcpListener;
+use std::io;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
+fn handle_stream(mut stream: TcpStream) -> io::Result<()> {
+    stream.write_all(b"+PONG\r\n")?;
+    stream.flush()?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        handle_stream(stream?)?;
     }
+
+    Ok(())
 }
